ddc643d45cbe7cc33abaf305995a3e6c7a875405
Eilif <Eilif>
2017-03-11 17:17:31 +0100

    created program file

---
+++ RobotArm.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+//create servo object
+Servo base;
+Servo joint1;
+Servo joint2;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
